a156550222fac912f56dbdc21c80350191590e56
levifvy <levifv.madrid@gmail.com>
2023-06-04 19:41:48 +0200

    improving an doing test with functions fron User and Game Controller

--- tests/Feature/GameControllerTest.php
+++ tests/Feature/GameControllerTest.php
@@ -7,81 +7,103 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Game;
 
-class GameControllerTest extends TestCase
-{
-    use RefreshDatabase;
-
-    /**
-     * A basic feature test example.
-     */
-    /** @test */ 
-    public function test_example(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-
-    <?php
-
-namespace Tests\Feature;
-
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
-
-class GameControllerTest extends TestCase
-{
-    use RefreshDatabase;
-
+use App\Models\User;
     
-    /** @test */
-    public function test_Create_Game()
-    {
-        $response = $this->post('/game', [
-            'title' => 'New Game',
-            'description' => 'This is a new game.',
-        ]);
 
-        $response->assertStatus(201);
-        $this->assertDatabaseHas('games', [
-            'title' => 'New Game',
-            'description' => 'This is a new game.',
-        ]);
+class GameControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+        /**
+         * A basic feature test example.
+         */
+        /** @test */ 
+        public function test_example(): void
+        {
+            $response = $this->get('/');
+
+            $response->assertStatus(200);
+        }
+    
+        /** @test */
+        public function test_list_throwed_games()
+        {
+            $user = User::factory()->create();
+    
+            $response = $this->actingAs($user)
+                ->get(route('players.listThrowedGames', $user->id));
+    
+            $response->assertStatus(200)
+                ->assertJson([
+                    'player' => $user->nickname,
+                    'average_rate' => $user->rate,
+                    'all_games_from_this_player' => $user->games,
+                ]);
+        }
+    
+        /** @test */
+        public function test_throwing_dices()
+        {
+            $user = User::factory()->create();
+    
+            $response = $this->actingAs($user)
+                ->get(route('games.throw', $user->id));
+    
+            $response->assertStatus(200)
+                ->assertJsonStructure([
+                    'Result of Throwing',
+                    'dice_1',
+                    'dice_2',
+                ]);
+        }
+    
+        /** @test */
+        public function test_delete_all_throws_of_a_player()
+        {
+            $user = User::factory()->create();
+    
+            $response = $this->actingAs($user)
+                ->delete(route('players.deleteAllThrowsOfAPlayer', $user->id));
+    
+            $response->assertStatus(200)
+                ->assertJson([
+                    'message' => 'The games belonging to ' . $user->nickname . ' have been deleted.',
+                ]);
+        }
+    
+        /** @test */
+        public function test_index_ranking()
+        {
+            $response = $this->get(route('players.indexRanking'));
+    
+            $response->assertStatus(200)
+                ->assertJsonStructure([
+                    'average_rate_throws_won',
+                ]);
+        }
+    
+        /** @test */
+        public function test_winner_ranking()
+        {
+            $response = $this->get(route('players.winnerRanking'));
+    
+            $response->assertStatus(200)
+                ->assertJsonStructure([
+                    'user_with_the_best_average',
+                    'rate',
+                ]);
+        }
+    
+        /** @test */
+        public function test_loser_ranking()
+        {
+            $response = $this->get(route('players.loserRanking'));
+    
+            $response->assertStatus(200)
+                ->assertJsonStructure([
+                    'user_with_the_worst_average',
+                    'rate',
+                ]);
+        }
     }
-
-   
-    /** @test */
-    public function test_Update_Game()
-    {
-        $game = factory(Game::class)->create();
-
-        $response = $this->put('/game/' . $game->id, [
-            'title' => 'Updated Game',
-            'description' => 'This game has been updated.',
-        ]);
-
-        $response->assertStatus(200);
-        $this->assertDatabaseHas('games', [
-            'id' => $game->id,
-            'title' => 'Updated Game',
-            'description' => 'This game has been updated.',
-        ]);
-    }
-
-    /** @test */
-    public function test_Delete_Game()
-    {
-        $game = factory(Game::class)->create();
-
-        $response = $this->delete('/game/' . $game->id);
-
-        $response->assertStatus(204);
-        $this->assertDatabaseMissing('games', [
-            'id' => $game->id,
-        ]);
-    }
-}
-
-
-}
+    
